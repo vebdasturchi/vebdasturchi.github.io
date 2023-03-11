5e52fe94a1075568e8e7066b794e65733d2a37fb
vebdasturchi <avazjonxamrokulov777@gmail.com>
2023-03-11 15:48:35 +0500

    My Portfolio

---
+++ portfolio/Hotels/js/script.js
@@ -0,0 +1,11 @@
+var nav = document.getElementById("nav");
+
+function open_btn() {
+    nav.style.left = "0";
+    nav.style.opacity = "1";
+}
+
+function close_btn() {
+    nav.style.left = "-200px";
+    nav.style.opacity = "0";
+}
